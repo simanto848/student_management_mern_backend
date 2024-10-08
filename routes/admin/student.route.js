deb82939bd53255d6b8a47c13346a55ff3bec434
simanto848 <simanto848@gmail.com>
2024-10-08 15:15:24 +0600

    show batch-wise students route

--- routes/admin/student.route.js
+++ routes/admin/student.route.js
@@ -6,6 +6,7 @@ import {
   update,
   remove,
   getStudentByKeyword,
+  getStudentsByBatch,
 } from "../../app/controllers/admin/student.controller.js";
 import { verifyUser } from "../../app/middlewares/verifyUser.js";
 
@@ -14,6 +15,7 @@ const router = express.Router();
 router.post("/", verifyUser, create);
 router.get("/", verifyUser, findAll);
 router.get("/search", verifyUser, getStudentByKeyword);
+router.get("/batch/:batchId/students", getStudentsByBatch);
 router.get("/:studentId", verifyUser, findOne);
 router.put("/:studentId", verifyUser, update);
 router.delete("/:studentId", verifyUser, remove);
